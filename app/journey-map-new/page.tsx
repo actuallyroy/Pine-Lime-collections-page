c35c39705135528a292bedc23e7e4d48761a6a4b
Amit Kumar <amitkumar93525@gmail.com>
2025-05-20 14:15:19 +0530

    fix: Update URL structure in JourneyMapPage for cart functionality
    - Modified the window.open call to use the updated URL format for journey map data, ensuring proper encoding and consistency when opening the shopping cart.

--- app/journey-map-new/page.tsx
+++ app/journey-map-new/page.tsx
@@ -194,9 +194,7 @@ export default function JourneyMapPage() {
         "mapData": _mapData,
         "product": "JOURNEY_MAP_V2"
       }
-
-      window.open(`https://www.pinenlime.com/shoppingcart?siteRevision=2&branchId=2f861fd5-89a0-4e44-9a6a-347dfaac6780&journeyMapData=${encodeURIComponent(btoa(encodeURIComponent(JSON.stringify(productData))))}`, '_blank');
-      // window.open(`https://www.pinenlime.com/shoppingcart?journeymapdata=${encodeURIComponent(btoa(encodeURIComponent(JSON.stringify(productData))))}`, '_blank');
+      window.open(`https://www.pinenlime.com/shoppingcart?journeymapdata=${encodeURIComponent(btoa(encodeURIComponent(JSON.stringify(productData))))}`, '_blank');
     }
   };
 
